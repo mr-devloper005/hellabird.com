437711b5f8cb4bd847ebe22a091a71c895921308
arti7652 <artirajput7652@gmail.com>
2026-07-13 09:59:29 +0530

    Update: about page, Ads add

--- src/editable/pages/HomePage.tsx
+++ src/editable/pages/HomePage.tsx
@@ -8,6 +8,7 @@ import type { SitePost } from '@/lib/site-connector'
 import { EditableHomeCta, EditableHomeHero, EditableMagazineSplit, EditableStoryRail, EditableTimeCollections } from '@/editable/sections/HomeSections'
 import { EditableSiteShell } from '@/editable/shell/EditableSiteShell'
 import { slot4BrandConfig } from '@/editable/theme/brand.config'
+import Ads from '@/lib/ads/ads'
 
 export const revalidate = 300
 
@@ -54,6 +55,9 @@ export default async function HomePage() {
         }}
       />
       <EditableHomeHero primaryTask={primaryTask} primaryRoute={primaryRoute} posts={primaryPosts} timeSections={timeSections} />
+      <div className="mx-auto max-w-6xl px-4 py-6">
+        <Ads slot="header" showLabel eager className="mx-auto w-full" />
+      </div>
       <EditableStoryRail primaryTask={primaryTask} primaryRoute={primaryRoute} posts={primaryPosts} timeSections={timeSections} />
       <EditableMagazineSplit primaryTask={primaryTask} primaryRoute={primaryRoute} posts={primaryPosts} timeSections={timeSections} />
       <EditableTimeCollections primaryTask={primaryTask} primaryRoute={primaryRoute} posts={primaryPosts} timeSections={timeSections} />
